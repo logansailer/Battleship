d4fc90b45ad0ef2e7035476c765481cf998afec1
Logan <logansailer0633@gamil.com>
2024-04-15 23:16:00 +0900

    finished class

---
+++ src/scripts/ship.js
@@ -0,0 +1,20 @@
+class Ship {
+  constructor(length) {
+    this.length = length;
+    this.timesHit = 0;
+  }
+
+  hit() {
+    this.timesHit++
+    }
+  
+
+  isSunk() {
+    if (this.timesHit >= this.length) {
+      return true;
+    }
+    return false;
+  }
+}
+
+module.exports = Ship;
