2e7df86a649b1cd0aae217220da4727dee242a76
Logan <logansailer0633@gamil.com>
2024-04-15 23:16:23 +0900

    work on game board size

--- src/scripts/gameboard.js
+++ src/scripts/gameboard.js
@@ -1,13 +1,31 @@
+const Ship = require("./ship");
+
 class Gameboard {
-    constructor(){
-
+  constructor() {
+    this.board = [];
+    for (let i = 0; i < 10; i++) {
+      for (let j = 0; j < 10; j++) {
+        this.board.push([i, j]);
+      }
     }
+  }
 
-    place(positionStart, positionEnd){
-        
+  place(position) {
+    if (
+      position[0] < 0 ||
+      position[0] > 9 ||
+      position[1] < 0 ||
+      position[1] > 9
+    ) {
+      return null;
     }
+    new Ship();
+  }
 
-    receiveAttack(){}
+  receiveAttack() {}
 
-    victory(){}
+  victory() {}
 }
+
+let board1 = new Gameboard();
+console.log(board1.board[]);
