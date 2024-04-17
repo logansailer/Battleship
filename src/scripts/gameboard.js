b1ed9651a457613c27ec5449cb0d53b361ce51b9
Logan <logansailer0633@gamil.com>
2024-04-17 20:17:52 +0900

    changed way user inputs corrdinates

--- src/scripts/gameboard.js
+++ src/scripts/gameboard.js
@@ -8,28 +8,50 @@ class Gameboard {
   constructor() {
     this.board = new Array(10).fill("0").map(() => new Array(10).fill("0"));
   }
-  
-  //takes an array as an arguement, places ship in that square
-  place(position) {
-    if (
-      position[0] < 0 ||
-      position[0] > 9 ||
-      position[1] < 0 ||
-      position[1] > 9
-    ) {
-      return null;
+
+  //places ship in starting position and the positions next to it given length and direction
+  place(x, y, length, dir) {
+    let ship = new Ship(length);
+    if (dir === "hor") {
+      for (let i = 0; i < length; i++) {
+        this.board[x][y + i] = 1;
+      }
     }
-    //new Ship(position);
-    console.log(position[0], position[1]);
-    this.board[position[0]][position[1]] = 1;
+    if (dir === "ver") {
+      for (let i = 0; i < length; i++) {
+        this.board[x + i][y] = 1;
+      }
+    }
+    // if (
+    //   position[0] < 0 ||
+    //   position[0] > 9 ||
+    //   position[1] < 0 ||
+    //   position[1] > 9
+    // ) {
+    //   return null;
+    // }
+  }
+
+  allSunk() {
+    //if all ships == sunk, return true
+    return false;
+  }
+
+  decideWinner() {
+    let winner = null;
+    //if all p1 ships sunk, winner = p2, else p1;
   }
 
   // if position has a ship, return true
   receiveAttack(coordinate) {
-    if (this.board[coordinate] === 1) {
-      Ship.hit();
+    if (this.board[coordinate[0]][coordinate[1]] === 1) {
+      Ship.hit(); //need to give ship a new variable
       return true;
     }
+    if (allSunk() === true) {
+      return decideWinner();
+    }
+    return false;
   }
 
   victory() {}
@@ -37,8 +59,7 @@ class Gameboard {
 
 let board1 = new Gameboard();
 
-board1.place([0, 1]);
-
-console.log(board1.board[0][1]);
+board1.place(0, 1, 3, "hor");
+console.log(board1.board[0][4]);
 
 module.exports = Gameboard;
