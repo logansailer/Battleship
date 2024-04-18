fe52d7b26ddf9bbbc6ec741821e4fb55e937db37
Logan <logansailer0633@gamil.com>
2024-04-18 22:51:42 +0900

    fixed tests to adjust to my flipping of x-y axis

--- src/tests/player.test.js
+++ src/tests/player.test.js
@@ -6,8 +6,14 @@ test("adds ship at given coordinate of given size and dirction from player", () 
   testPlayer.game.place(1, 1, 4, "hor");
   expect(
     testPlayer.game.board[1][1] &&
-    testPlayer.game.board[1][2] &&
-    testPlayer.game.board[1][3] &&
-    testPlayer.game.board[1][4]
+      testPlayer.game.board[2][1] &&
+      testPlayer.game.board[3][1] &&
+      testPlayer.game.board[4][1]
   ).toBe(1);
+});
+
+test("checks if ship is added to ships array", () => {
+  testPlayer.game.place(1, 1, 4, "hor");
+  testPlayer.game.place(1, 2, 4, "hor");
+  expect(testPlayer.game.ships).toEqual([4, 4]);
 });
