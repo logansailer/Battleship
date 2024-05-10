0fc77e3c6d6eca220cd977364881bd5f058a07e8
Logan Sailer <logansailer0633@gmail.com>
2024-05-10 13:30:46 +0900

    dev server fix

--- webpack.config.js
+++ webpack.config.js
@@ -14,6 +14,9 @@ module.exports = {
     path: path.resolve(__dirname, "dist"),
     filename: "index.js",
   },
+  devServer: {
+    contentBase: "./dist",
+  },
   optimization: {
     minimize: false,
   },
